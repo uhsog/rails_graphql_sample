2dfea2e10c4705697bcaa1f59b0b0ed2965ad252
uhsog <30695524+uhsog@users.noreply.github.com>
2020-09-06 22:05:29 +0900

    Add Mutation for UpdateTask

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :update_task, mutation: Mutations::UpdateTask
     field :create_task, mutation: Mutations::CreateTask
     # TODO: remove me
     field :test_field, String, null: false,
